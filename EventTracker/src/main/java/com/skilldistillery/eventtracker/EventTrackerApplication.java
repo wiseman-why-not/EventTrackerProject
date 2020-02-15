2aabc12dc7d57fc8d42845712f494e97a40fd2e8
wiseman-why-not <marcus.e.wiseman@gmail.com>
2020-02-14 19:27:41 -0700

    stubbed out index.html

--- EventTracker/src/main/java/com/skilldistillery/eventtracker/EventTrackerApplication.java
+++ EventTracker/src/main/java/com/skilldistillery/eventtracker/EventTrackerApplication.java
@@ -2,12 +2,19 @@ package com.skilldistillery.eventtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EventTrackerApplication {
+public class EventTrackerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventTrackerApplication.class, args);
 	}
+	
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(EventTrackerApplication.class);
+	  }
 
 }
