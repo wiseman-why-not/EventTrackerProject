78ea2a1d6b4db295ce8871f7b4baadbd2e475325
wiseman-why-not <marcus.e.wiseman@gmail.com>
2020-02-15 19:09:43 -0700

    deleted some empty spaces and some console logging

--- EventTracker/src/main/resources/static/js/script.js
+++ EventTracker/src/main/resources/static/js/script.js
@@ -1,19 +1,22 @@
-window.addEventListener('load', function(e) {
+window.addEventListener('load', function (e) {
 	console.log('document loaded');
 	init();
-	
+
 });
 
 function init() {
-	document.crimeForm.lookup.addEventListener('click', function(event) {
+	document.crimeForm.lookup.addEventListener('click', function (event) {
 		event.preventDefault();
 		var crimeId = document.crimeForm.crimeId.value;
 		if (!isNaN(crimeId) && crimeId > 0) {
 			getCrime(crimeId);
+			// resets the form after you search
+			let form = event.target.parentElement;
+			form.reset();
 		}
 	})
 
-	document.createCrimeForm.create.addEventListener('click', function(event) {
+	document.createCrimeForm.create.addEventListener('click', function (event) {
 		event.preventDefault();
 		let newCrime = {};
 		let crName = document.createCrimeForm.crimeName.value;
@@ -30,18 +33,15 @@ function init() {
 	getCrimes();
 }
 
-
-
 function getCrime(crimeId) {
 	let xhr = new XMLHttpRequest();
 
 	xhr.open('GET', 'api/crimes/' + crimeId);
 
-	xhr.onreadystatechange = function() {
+	xhr.onreadystatechange = function () {
 		// If status is below error range, and readyState is 4 (DONE)
 		if (xhr.readyState === 4 && xhr.status < 400) {
 			var crime = JSON.parse(xhr.responseText);
-			console.log(crime);
 			crimeDetail(crime);
 		}
 
@@ -54,54 +54,6 @@ function getCrime(crimeId) {
 	};
 
 	xhr.send(null);
-}
-
-function displayCrime(crime) {
-	let crimeDiv = document.getElementById('crimeData');
-	crimeDiv.textContent = '';
-
-	let h2Title = document.createElement('h2');
-	h2Title.textContent = crime.crimeName;
-	crimeDiv.appendChild(h2Title);
-
-	let neighborhood = document.createElement('p');
-	neighborhood.textContent = crime.neighborhood;
-	crimeDiv.appendChild(neighborhood);
-
-
-	let btnBack = document.createElement('button');
-	btnBack.textContent = "Go Back";
-	
-	// edit and back button event listeners
-	let btnEdit = document.createElement('button');
-	btnEdit.textContent = "Edit Crime";
-
-	let btnDelete = document.createElement('button');
-	btnDelete.textContent = "Delete Crime";
-	
-	btnBack.addEventListener('click', function(e){
-		var crimeTable = document.getElementById('allCrimeData');
-			if (crimeTable !== null){
-				crimeTable.remove();
-				getCrimes();
-			}
-	});
-	
-	btnEdit.addEventListener('click',function(e){
-		editCrime(crime);
-		getCrimes();
-	});
-
-	btnDelete.addEventListener('click',function(e){
-		e.preventDefault();
-		deleteCrime(crime);
-		getCrimes();
-	});
-
-	crimeDiv.appendChild(btnBack);
-	crimeDiv.appendChild(btnEdit);
-	crimeDiv.appendChild(btnDelete);
-
 }
 
 function displayAllCrimes(crimes) {
@@ -133,7 +85,7 @@ function displayAllCrimes(crimes) {
 
 	let tbody = document.createElement('tbody');
 
-	allCrimes.forEach(function(value, index, array) {
+	allCrimes.forEach(function (value, index, array) {
 
 		let tbodyRow = document.createElement('tr');
 
@@ -143,10 +95,10 @@ function displayAllCrimes(crimes) {
 
 		tbodyData1.textContent = value.id;
 		tbodyRow.appendChild(tbodyData1);
-		
+
 
 		tbodyData2.textContent = value.crimeName;
-		tbodyData2.addEventListener('click', function(e){
+		tbodyData2.addEventListener('click', function (e) {
 			crimeDetail(value);
 		});
 		tbodyRow.appendChild(tbodyData2);
@@ -162,8 +114,8 @@ function displayAllCrimes(crimes) {
 
 }
 
-function crimeDetail(crime){
-	
+function crimeDetail(crime) {
+
 	let singleCrimeData = document.getElementById('singleCrimeData');
 	singleCrimeData.textContent = '';
 
@@ -174,35 +126,35 @@ function crimeDetail(crime){
 	let neighborhood = document.createElement('p');
 	neighborhood.textContent = crime.neighborhood;
 	singleCrimeData.appendChild(neighborhood);
-	
+
 	let btnBack = document.createElement('button');
 	btnBack.textContent = "Go Back";
-	
+
 	// edit and back button event listeners
 	let btnEdit = document.createElement('button');
 	btnEdit.textContent = "Edit Crime";
 
 	let btnDelete = document.createElement('button');
 	btnDelete.textContent = "Delete Crime";
-	
-	btnBack.addEventListener('click', function(e){
+
+	btnBack.addEventListener('click', function (e) {
 		singleCrimeData.textContent = '';
 		getCrimes();
 	});
-	
-	btnEdit.addEventListener('click',function(e){
+
+	btnEdit.addEventListener('click', function (e) {
 		editCrime(crime);
 		getCrimes();
-		
+
 	});
 
-	btnDelete.addEventListener('click',function(e){
+	btnDelete.addEventListener('click', function (e) {
 		e.preventDefault();
 		deleteCrime(crime);
 		singleCrimeData.textContent = '';
 		getCrimes();
 	});
-	
+
 	// append the buttons to the crime detail
 	singleCrimeData.appendChild(btnBack);
 	singleCrimeData.appendChild(btnEdit);
@@ -211,7 +163,7 @@ function crimeDetail(crime){
 
 }
 
-function editCrime(crimeObject){
+function editCrime(crimeObject) {
 
 	let editCrimeDataDiv = document.getElementById('editCrimeData');
 
@@ -219,6 +171,7 @@ function editCrime(crimeObject){
 	var form = document.createElement('form');
 	form.name = 'editForm';
 
+	// fields for form
 	var crimeIdInput = document.createElement('input');
 	crimeIdInput.name = 'id'; // assign a name attribute
 	crimeIdInput.type = 'hidden'; // assign a type attribute
@@ -241,35 +194,30 @@ function editCrime(crimeObject){
 	form.appendChild(crimeNeighborhoodInput);
 
 	var submit = document.createElement('input');
-		submit.name = 'submit'; // assign a name attribute
-		submit.type = 'submit'; // assign a type attribute
-		submit.value = 'Submit Form'; // assign a value attribute
+	submit.name = 'submit'; // assign a name attribute
+	submit.type = 'submit'; // assign a type attribute
+	submit.value = 'Submit Form'; // assign a value attribute
 
-		submit.addEventListener('click', function(e) { // Assign an event listener to the submit button variable
+	submit.addEventListener('click', function (e) { // Assign an event listener to the submit button variable
 
-			e.preventDefault();
-			var form = e.target.parentElement;
-			console.log(form);
-			
-			// print the crimeNameInput value to the console
-			console.log(form.id.value);
-			console.log(form.crimeName.value);
-			console.log(form.neighborhood.value);
-			// reassign the crime with updated info
-			crimeObject.crimeName = form.crimeName.value;
-			crimeObject.neighborhood = form.neighborhood.value;
+		e.preventDefault();
+		var form = e.target.parentElement;
 
-			// PUT request, update crime
-			updateCrime(crimeObject);
-			editCrimeData.textContent = '';
-			let singleCrimeData = document.getElementById('singleCrimeData');
-	singleCrimeData.textContent = '';
-			// clear the form data
-			form.reset();
-			getCrimes();
-		});
+		// reassign the crime with updated info
+		crimeObject.crimeName = form.crimeName.value;
+		crimeObject.neighborhood = form.neighborhood.value;
+
+		// PUT request, update crime
+		updateCrime(crimeObject);
+		editCrimeData.textContent = '';
+		let singleCrimeData = document.getElementById('singleCrimeData');
+		singleCrimeData.textContent = '';
+		// clear the form data
+		form.reset();
+		getCrimes();
+	});
 	form.appendChild(submit);
-	editCrimeDataDiv.appendChild(form);	
+	editCrimeDataDiv.appendChild(form);
 }
 
 // GET display all crimes
@@ -278,7 +226,7 @@ function getCrimes() {
 
 	xhr.open('GET', 'api/crimes/');
 
-	xhr.onreadystatechange = function() {
+	xhr.onreadystatechange = function () {
 		// If status is below error range, and readyState is 4 (DONE)
 		if (xhr.readyState === 4 && xhr.status < 400) {
 			var crimes = JSON.parse(xhr.responseText);
@@ -304,11 +252,10 @@ function postCrime(crimeObject) {
 	xhr.setRequestHeader("Content-type", "application/json"); // Specify JSON
 	// request body
 
-	xhr.onreadystatechange = function() {
+	xhr.onreadystatechange = function () {
 		if (xhr.readyState === 4) {
 			if (xhr.status == 200 || xhr.status == 201) { // Ok or Created
 				var crime = JSON.parse(xhr.responseText);
-				console.log(crime);
 				getCrimes();
 			} else {
 				console.log("POST request failed.");
@@ -327,18 +274,17 @@ function postCrime(crimeObject) {
 
 function updateCrime(crimeObject) {
 	let xhr = new XMLHttpRequest();
-	xhr.open('Put', 'api/crimes/' +crimeObject.id , true);
+	xhr.open('Put', 'api/crimes/' + crimeObject.id, true);
 
 	xhr.setRequestHeader("Content-type", "application/json"); // Specify JSON
 	// request body
 
-	xhr.onreadystatechange = function() {
+	xhr.onreadystatechange = function () {
 		if (xhr.readyState === 4) {
 			if (xhr.status == 200 || xhr.status == 201) { // Ok or Created
 				var updatedCrime = JSON.parse(xhr.responseText);
-				console.log(updatedCrime);
 				getCrimes();
-				
+
 			} else {
 				console.log("PUT request failed.");
 				console.error(xhr.status + ': ' + xhr.responseText);
@@ -354,14 +300,14 @@ function updateCrime(crimeObject) {
 
 // DELETE crime
 
-function deleteCrime(crimeObject){
+function deleteCrime(crimeObject) {
 	let xhr = new XMLHttpRequest();
-	xhr.open('Delete', 'api/crimes/' + crimeObject.id , true);
+	xhr.open('Delete', 'api/crimes/' + crimeObject.id, true);
 
 	xhr.setRequestHeader("Content-type", "application/json"); // Specify JSON
 	// request body
 
-	xhr.onreadystatechange = function() {
+	xhr.onreadystatechange = function () {
 		if (xhr.readyState === 4) {
 			if (xhr.status == 200 || xhr.status == 201) { // Ok or Created
 				console.log("successfully deleted");
